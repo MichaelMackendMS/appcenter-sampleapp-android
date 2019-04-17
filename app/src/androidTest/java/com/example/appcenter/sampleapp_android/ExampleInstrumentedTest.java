5d64dab6845221284e527c8ccaf65bb12d9bd8af
MichaelMackendMS <michael.mackend@microsoft.com>
2019-04-17 16:53:20 -0700

    update build.grade to use espresso test extensions and implement the expected api scaffolding so as to get a passing test in AppCenter

--- app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
@@ -6,8 +6,12 @@ import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.After;
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +20,23 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
+        reportHelper.label("Testing useAppContext...");
+
         Context appContext = InstrumentationRegistry.getTargetContext();
+        android.util.Log.d("message: ", "Checking use app context!");
 
         assertEquals("com.example.appcenter.sampleapp_android", appContext.getPackageName());
+    }
+
+    @After
+    public void TearDown(){
+        reportHelper.label("Stopping App");
     }
 }
